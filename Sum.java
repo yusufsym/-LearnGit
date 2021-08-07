9f69704f2cfe606a605f22d219d295f11c2bbfdb
yusufsym <yusuf.sayed@neosoftmail.com>
2021-08-07 12:05:04 +0530

    Initial commit

---
+++ Sum.java
@@ -0,0 +1,7 @@
+public class Sum{
+            public static void main (String a[]){
+                        int num1 =5, num2 = 15, sum:
+                        sum= num1+num2;
+                        System.out.println("sum of these number" +sum);
+}
+}
