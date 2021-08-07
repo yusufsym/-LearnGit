954ff933205ed9d72465e49b8d042c80a0e5c331
yusufsym <yusuf.sayed@neosoftmail.com>
2021-08-07 12:21:53 +0530

    Message changed in Sum.java

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum{
             public static void main (String a[]){
                         int num1 =5, num2 = 15, sum:
                         sum= num1+num2;
-                        System.out.println("sum of these number" +sum);
+                        System.out.println("Sum of number is : " +sum);
 }
 }
